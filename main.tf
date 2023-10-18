0f795212d8a72bd9acda14e5134cf517b91a25e2
Igne <125908665+Ignejoneigne@users.noreply.github.com>
2023-10-18 12:01:43 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+#provider "aws" {
+#  region = var.AWS_region
+#  profile = "default"
+#}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "eu-central-1"
+  profile = "default"
+}
+
+resource "aws_instance" "IgneJone_instance" {
+  ami           = var.AMI_ID
+  instance_type = var.INSTANCE_TYPE
+  iam_instance_profile = var.IAM_INSTANCE_PROFILE
+
+  user_data = file("user_data.sh")
+
+  tags = {
+    Name = "IgneJone"
+  }
+}
+
+resource "aws_s3_bucket" "IgneJone_bucket" {
+  bucket = var.S3_BUCKET_NAME
+
+  tags = {
+    Name = "IgneJone"
+  }
+}
